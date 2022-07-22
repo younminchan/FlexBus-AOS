fb2ee09c65a86fe2290850eb02ddf8c8ba8db9ef
younminchan <dbsalscks0@naver.com>
2022-07-22 17:41:18 +0900

    update 실시간 노선검색

--- app/src/main/java/com/lotterental/flexbus_aos/SearchFragment.kt
+++ app/src/main/java/com/lotterental/flexbus_aos/SearchFragment.kt
@@ -1,7 +1,8 @@
 package com.lotterental.flexbus_aos
 
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
@@ -20,6 +21,7 @@ import com.lotterental.flexbus_aos.viewmodel.MainViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 
 class SearchFragment : Fragment() {
     lateinit var binding: FragmentSearchBinding
@@ -30,7 +32,11 @@ class SearchFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
         mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
 
@@ -45,7 +51,7 @@ class SearchFragment : Fragment() {
             App.activity.onBackPressed()
         }
 
-        /** 노선번호 검색 */
+        /** 노선번호 검색버튼 */
         binding.tvSearchInput.setOnClickListener {
             var searchBusNum:String = binding.etSearch.text.toString()
             if(!searchBusNum.isNullOrEmpty()) {
@@ -54,19 +60,49 @@ class SearchFragment : Fragment() {
                 Toast.makeText(App.activity, "${searchBusNum} 노선번호 검색", Toast.LENGTH_SHORT).show()
             }
         }
-        binding.etSearch.setOnEditorActionListener(object : TextView.OnEditorActionListener {
-            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
-                if (actionId == EditorInfo.IME_ACTION_NEXT) {
-                    binding.tvSearchInput.performClick()
-                    Utils.hideKeyboard()
-                    return true
-                } else {
+        /** 노선번호 검색입력 */
+        binding.etSearch.apply {
+            setOnEditorActionListener(object : TextView.OnEditorActionListener {
+                override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                    if (actionId == EditorInfo.IME_ACTION_NEXT) {
+//                        binding.tvSearchInput.performClick() //강제클릭
+                        Utils.hideKeyboard()
+                        return true
+                    } else {
 
-                    return false
+                        return false
+                    }
                 }
-            }
-        })
-        binding.etSearch.filters = arrayOf(Utils.filterAlphaNumSpace)
+            })
+
+            filters = arrayOf(Utils.filterAlphaNumSpace)
+
+            addTextChangedListener(object: TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                private var timer: Timer = Timer()
+                private val DELAY: Long = 1000 // Milliseconds
+
+                override fun afterTextChanged(s: Editable?) {
+                    timer.cancel()
+                    timer = Timer()
+                    timer.schedule(object : TimerTask() {
+                            override fun run() {
+                                // TODO: Do what you need here (refresh list).
+                                // You will probably need to use
+                                // runOnUiThread(Runnable action) for some
+                                // specific actions (e.g., manipulating views).
+
+                                var searchText = binding.etSearch.text
+                                if(!searchText.isNullOrEmpty()){
+                                    getBusRouteList("${searchText}")
+                                }
+                            }
+                        }, DELAY)
+                }
+            })
+        }
 
         /** 버스정류소 RecyClerVIew */
         initRecyclerViewBusSearch()
@@ -82,7 +118,10 @@ class SearchFragment : Fragment() {
 
     private fun getBusRouteList(strSrch: String) {
         MainRepository().getBusRouteList(strSrch).enqueue(object : Callback<BusRouteListItem> {
-            override fun onResponse(call: Call<BusRouteListItem>, response: Response<BusRouteListItem>, ) {
+            override fun onResponse(
+                call: Call<BusRouteListItem>,
+                response: Response<BusRouteListItem>,
+            ) {
                 if(response.isSuccessful){
                     var res = response.body()
 //                Log.e("YMC", "retrofit res: ${res}")
@@ -90,7 +129,7 @@ class SearchFragment : Fragment() {
 //                Log.e("YMC", "retrofit res item: ${res?.msgBody?.itemList}")
 
 //                res?.msgBody?.itemList?.forEach {
-//                    Log.e("YMC","=====forRach: ${it}")
+//                    Log.e("YMC","=====forEach: ${it}")
 ////                    vusRouteItemList.add(it)
 //                }
 
